36ec2ccc50c7bec7a31710f6e56c8dd408ce9ef0
sabbirsam <samiubat14@gmail.com>
2022-12-13 12:25:04 +0600

    end this project for now

--- build/pro_index.asset.php
+++ build/pro_index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'a481cefc1ac13f1e6c61');
+<?php return array('dependencies' => array('wp-element'), 'version' => '92050c906f38a7f2fd97');
