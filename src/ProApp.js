def1ca9a106df1401c5724e9ef64e67f73d4dcd2
sabbirsam <samiubat14@gmail.com>
2023-01-05 00:14:48 +0600

    Update

--- src/ProApp.js
+++ src/ProApp.js
@@ -1,7 +1,6 @@
 import "./app.scss";
 const ProApp = () => {
     let current_page = window.location.search;
-
     if(current_page === '?page=wcs_documentation'){
         return (
             <div id="wcs_documentation">Documentation page showing from pro plugin</div>
